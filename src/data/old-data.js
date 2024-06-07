582c744f4fc1a6642b53e2787fc9e235ee45a0c8
Ankit Verma <ankitv78389@gmail.com>
2024-06-07 19:26:34 +0530

    :hammer:[refractor][addded new projects to the portfolio]

---
+++ src/data/old-data.js
@@ -0,0 +1,28 @@
+// Old-projects--->
+
+const oldData = [
+  {
+    cover: '/portfolio-images/klingon-talk.png',
+    title: 'Klingon Speak',
+    description: 'HTML5,CSS3, Javascript, API',
+    url: 'https://english-2-klingon.netlify.app/',
+  },
+  {
+    cover: '/portfolio-images/marvel-quiz2.png',
+    title: 'Marvel-Fandom Quiz',
+    description: 'CLI App, NodeJS',
+    url: 'https://replit.com/@AnkitVerma12/marvel-fandom-quiz#index.js?embed=1&output=1',
+  },
+  {
+    cover: '/portfolio-images/doyouknowme.png',
+    title: 'How well do you know me?',
+    description: 'CLI App, NodeJS',
+    url: 'https://replit.com/@AnkitVerma12/How-well-do-your-friends-know-you??embed=1&output=1',
+  },
+  {
+    cover: '/portfolio-images/old-portfolio.png',
+    title: 'Old Porfolio',
+    description: 'HTML5, CSS3',
+    url: 'https://ankit-verma.netlify.app/',
+  },
+]
